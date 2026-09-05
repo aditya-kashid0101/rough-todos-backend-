0ee6db6525fb895cd20d1e651d321b17841a6321
a-dityaCV <aditya.kashid0101@gmail.com>
2026-09-05 16:09:54 +0530

    incomplete todos backend

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://xyzxyz.ge6ty8m.mongodb.net/AdityaDb",
+  )
+  .then(() => console.log("Connected!"));
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+const User = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  name: String,
+});
+
+const Todo = new Schema({
+  title: String,
+  done: Boolean,
+  userId: ObjectId,
+});
+
+const UserModel = mongoose.model("users", User);
+const TodoModel = mongoose.model("todos", Todo);
+
+module.exports = {
+  UserModel: UserModel,
+  TodoModel: TodoModel,
+};
